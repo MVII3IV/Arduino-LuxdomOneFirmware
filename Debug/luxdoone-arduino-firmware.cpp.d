f45d909ed219b7baf1365a30680fe04e5acec222
MVII3IV <afrosclothing@hotmail.com>
2017-10-28 10:04:35 -0600

    functional project

---
+++ Debug/luxdoone-arduino-firmware.cpp.d
@@ -0,0 +1,91 @@
+C:\Users\MVII3IV\AppData\Local\Temp\VMBuilds\luxdoone-arduino-firmware\uno\Debug\luxdoone-arduino-firmware.cpp.o: \
+ C:\Users\MVII3IV\AppData\Local\Temp\VMBuilds\luxdoone-arduino-firmware\uno\Debug\luxdoone-arduino-firmware.cpp \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.h \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/DynamicJsonBuffer.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonBufferBase.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/JsonParser.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../JsonBuffer.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/NonCopyable.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../JsonVariant.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/JsonVariantContent.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/JsonFloat.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/../Configuration.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/JsonInteger.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/JsonVariantDefault.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/JsonVariantType.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../JsonVariantBase.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Data/JsonVariantAs.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Polyfills/attributes.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/JsonPrintable.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../TypeTraits/EnableIf.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/DummyPrint.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/DynamicStringBuilder.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/StringTraits.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/../TypeTraits/IsBaseOf.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/../TypeTraits/IsChar.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/../TypeTraits/IsSame.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/../TypeTraits/RemoveReference.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/ArduinoStream.hpp \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/CharPointer.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/FlashString.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/StdStream.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../StringTraits/StdString.hpp \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/IndentedPrint.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/JsonSerializer.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/JsonWriter.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../Data/Encoding.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../Polyfills/math.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../Polyfills/normalize.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/../TypeTraits/FloatTraits.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/Prettyfier.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../Serialization/StaticStringBuilder.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../RawJson.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/IsFloatingPoint.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/IsIntegral.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/IsSignedIntegral.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/IsUnsignedIntegral.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/RemoveConst.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/IsArray.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/../TypeTraits/IsConst.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/StringWriter.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonArray.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/JsonBufferAllocated.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/List.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/ListConstIterator.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/ListNode.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/ListIterator.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/ReferenceType.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/ValueSetter.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonObject.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonPair.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonVariantComparisons.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Data/JsonVariantComparer.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/StaticJsonBuffer.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/JsonParserImpl.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Deserialization/Comments.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonArrayImpl.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonArraySubscript.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonBufferImpl.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonObjectImpl.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonObjectSubscript.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonVariantImpl.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/isFloat.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/./ctype.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/isInteger.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/parseFloat.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/parseInteger.hpp \
+ C:\Users\MVII3IV\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Serialization/JsonSerializerImpl.hpp
---
+++ luxdoone-arduino-firmware.ino
@@ -0,0 +1,161 @@
+#include <ArduinoJson.h>
+String deviceId = "0013A200_40EAE365";
+int numberOfRelays = 0;
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+//SERIAL CONFIGURATIONS
+int BAUDRATE = 9600;
+
+//RELAYS
+int  output01 = 2; //0
+int  output02 = 3; //1
+int  output03 = 4; //2
+int  output04 = 5; //3
+
+int  output05 = 6; //4
+int  output06 = 7; //5
+int  output07 = 8; //6
+int  output08 = 9; //7
+
+
+int relays[] = {
+	output01,
+	output02,
+	output03,
+	output04,
+
+	output05,
+	output06,
+	output07,
+	output08,
+};
+
+
+
+void setup() {
+
+	Serial.begin(BAUDRATE);
+	// reserve 200 bytes for the inputString:
+	inputString.reserve(200);
+
+	numberOfRelays = sizeof(relays) / sizeof(int);
+
+	Serial.println("Luxdom system loaded..");
+	Serial.println("Xbee ID : " + deviceId);
+
+
+	for (int i = 0; i < numberOfRelays; i++) {
+		pinMode(relays[i], OUTPUT);
+		digitalWrite(relays[i], LOW);
+	}
+
+	Serial.println("Number of relays : " + String(numberOfRelays));
+}
+
+void loop() {
+	if (stringComplete) {
+
+		//{"deviceId":"0013A200_40EAE365","relay":1,"instruction":0}
+		StaticJsonBuffer<100> jsonBuffer;
+		JsonObject& root = jsonBuffer.parseObject(inputString);
+		String id = root["id"];
+		int relay = root["rel"];
+		int instruction = root["ins"];
+		int type = root["typ"];
+
+		// clear the string:
+		inputString = "";
+		stringComplete = false;
+
+
+		if (!root.success()) {
+			Serial.println("parseObject() failed");
+			return;
+		}
+
+		if (deviceId != id) {
+			Serial.println("It's not my duty");
+			return;
+		}
+
+		if (relay < 0) {
+			Serial.println("Relay not valid : " + String(relay));
+			Serial.println("Number of relays : " + String(numberOfRelays));
+			return;
+		}
+
+		Serial.println("\n");
+		Serial.println("DeviceID : " + id);
+		Serial.println("Relay : " + String(relay));
+		Serial.println("Instruction : " + String(instruction));
+		Serial.println("type : " + String(type));
+
+		//turn on all relays
+		if (relay == 100) {
+			for (int x = 0; x < numberOfRelays; x++) {
+				digitalWrite(relays[x], 1);
+				delay(100);
+			}
+		}
+		//turn off all relays
+		else if (relay == 200) {
+			for (int x = 0; x < numberOfRelays; x++) {
+				digitalWrite(relays[x], 0);
+				delay(100);
+			}
+		}
+
+		switch (type) {
+			case 1 : standardDeviceOrder(relay, instruction); break;
+			case 2: pulseDeviceOrder(relay); break;
+		}
+		
+		sendResponse(relay, instruction);
+	}
+	delay(100);
+}
+
+void standardDeviceOrder(int relay, int instruction) {
+	digitalWrite(relays[relay], instruction);
+}
+
+void pulseDeviceOrder(int relay) {
+	digitalWrite(relays[relay], true);
+	delay(500);
+	digitalWrite(relays[relay], false);
+}
+
+void sendResponse(int relay, int state) {
+	StaticJsonBuffer<200> jsonBuff;
+
+	JsonObject& root = jsonBuff.createObject();
+	root["deviceId"] = deviceId;
+	root["relay"] = relay;
+	root["state"] = state;
+
+	root.printTo(Serial);
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+	while (Serial.available()) {
+		// get the new byte:
+		char inChar = (char)Serial.read();
+		// add it to the inputString:
+		inputString += inChar;
+		// if the incoming character is a newline, set a flag
+		// so the main loop can do something about it:
+		if (inChar == '\n') {
+			stringComplete = true;
+		}
+	}
+}
+
+
